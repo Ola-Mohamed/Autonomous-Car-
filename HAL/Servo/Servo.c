3b6e86195c549aefd31ed119ee7b8dbd59bd597e
Mostafa Abdelfatah <131954621+Mostafa-1997@users.noreply.github.com>
2023-12-23 23:38:53 +0200

    Final Servo Motor Code

--- HAL/Servo/Servo.c
+++ HAL/Servo/Servo.c
@@ -1,1 +1,32 @@
+/*
+ * Servo.c
+ *
+ *  Created on: Nov 6, 2023
+ *      Author: mosta
+ */
 
+#include "Servo.h"
+
+S_Dio S_Servo = { PORTE_ID, PIN4_ID, OUTPUT };
+
+void Servo_Init(void) {
+	Timer3_Init(TIMER3_FASTPWM_OCRA_TOP_MODE,TIMER3_SCALER_8);
+	MCAL_Dio_SetPinDirection(&S_Servo);
+	Timer3_OCRB3Mode(OCRB3_NON_INVERTING);
+	PWM_Freq_HZ(50);
+}
+
+void Servo_Move(sint16 angle) // angle*10 ----- To get high res //
+// angle range : -90 to 90 //
+{
+	sint16 r_angle = 0;
+	sint16 duty = 0;
+
+	if (angle >= -90 && angle <= 90) {
+		r_angle = 90 + angle;
+		duty = 50 + ((r_angle * 50) / 180); // step = 3.6°
+		PWM_Duty(duty);
+	} else {
+		// Do Nothing
+	}
+}
---
+++ HAL/Servo/Servo.h
@@ -0,0 +1,23 @@
+/*
+ * Servo.h
+ *
+ *  Created on: Nov 6, 2023
+ *      Author: mosta
+ */
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+#include"../../MCAL/Timer3/Timer3_Interface.h"
+#include "../../std_types.h"
+#include "../../MCAL/DIO/DIO_Interface.h"
+
+
+#define OPEN 90
+#define CLOSE -90
+
+
+void Servo_Init (void);
+void Servo_Move (sint16 angle);
+
+#endif /* SERVO_H_ */
